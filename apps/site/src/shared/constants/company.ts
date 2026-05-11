e5ab3448719de335d638491c7878a0d7b33bb7af
DenisVorop <saskeazaske@icloud.com>
2026-05-11 22:51:22 +0300

    fix(site): update support email

--- apps/site/src/shared/constants/company.ts
+++ apps/site/src/shared/constants/company.ts
@@ -6,7 +6,7 @@ export const companyDetails = {
   registrationNumberLabel: "ОГРНИП",
   registrationNumber: "324774600166803",
   registrationDate: "13.03.2024",
-  supportEmail: "artmate.official@outlook.com",
+  supportEmail: "support@art-mate.ru",
   businessHours: "Пн-пт, 10:00-18:00 по московскому времени",
   paymentProvider: "Ozon Pay",
   acquiringProvider: "Ozon Банк",
